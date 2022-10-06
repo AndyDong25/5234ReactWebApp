2c6a62d7aa9f22bc5c4b58052e8cb4e5185f613b
Andy Dong <dong.andy25@outlook.com>
2022-10-06 13:36:33 -0400

    Started worked on about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,28 +1,34 @@
 import React from "react";
-
+import './about.css'
+ 
 const About = () => {
   
   return (
-    <div>
+    <>
       <h1>
         About PFD
       </h1>
-      <h3>
-        include team member images, job titles, and description
-      </h3>
-      <h4>
-        Jason
-      </h4>
-      <p> </p>
-      <h4>
-        Andy
-      </h4>
-      <p> </p>
-      <h4>
-        JJ
-      </h4>
-      <p> </p>
-    </div>
+      <div class="grid-squares">
+        <div class="grid_item">
+          <img src = "https://png.pngtree.com/png-vector/20191101/ourlarge/pngtree-male-avatar-simple-cartoon-design-png-image_1934458.jpg" alt="avatar" width="150" height ="150"></img>
+          <h3>Title</h3>
+          <h4>Andy Dong</h4>
+          <p>Description</p>
+        </div>
+        <div class="grid_item">
+          <img src = "https://png.pngtree.com/png-vector/20191103/ourlarge/pngtree-handsome-young-guy-avatar-cartoon-style-png-image_1947775.jpg" alt="avatar" width="150" height ="150"></img>
+          <h3>Title</h3>
+          <h4>Jason</h4>
+          <p>Description</p>
+        </div>  
+        <div class="grid_item">
+          <img src = "https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg" alt="avatar" width="150" height ="150"></img>
+          <h3>Title</h3>
+          <h4>JJ</h4>
+          <p>Description</p>
+        </div>
+      </div>
+    </>
   );
 };
   
